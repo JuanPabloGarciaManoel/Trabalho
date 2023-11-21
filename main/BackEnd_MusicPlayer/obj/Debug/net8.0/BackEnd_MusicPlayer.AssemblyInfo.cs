4f1bcc72b7f78027f878de9bbefa14d157b8b77c
Juan <juan.manoel@viannasempre.com.br>
2023-11-21 12:12:12 -0300

    Ajuste

--- main/BackEnd_MusicPlayer/obj/Debug/net8.0/BackEnd_MusicPlayer.AssemblyInfo.cs
+++ main/BackEnd_MusicPlayer/obj/Debug/net8.0/BackEnd_MusicPlayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd_MusicPlayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4ac17b577701aaa4cd93263e296e24844635e54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd_MusicPlayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd_MusicPlayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- main/FrontEnd_MusicPlayer/src/components/Layout.js
+++ main/FrontEnd_MusicPlayer/src/components/Layout.js
@@ -32,27 +32,27 @@ const Layout = () => {
     return (
         <React.Fragment>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                            <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Cadastros
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="/comum">Comum</a><p></p>
-                                <a className="dropdown-item" href="/administrador">Administrador</a>
-                                { cadastrarAdmin }
-                            </div>
-                        </li>
-                        {usuarioIdentificacao}
-                        {loginLogout}
-                    </ul>
-                </div>
-            </nav>      
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto d-flex align-items-center">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Cadastros
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="/comum">Comum</a><p></p>
+          <a className="dropdown-item" href="/administrador">Administrador</a>
+          {cadastrarAdmin}
+        </div>
+      </li>
+      {usuarioIdentificacao}
+      {loginLogout}
+    </ul>
+  </div>
+</nav>
+  
             <Outlet />
         </React.Fragment>
     );
